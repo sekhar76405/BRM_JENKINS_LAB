d0435d08dcb1152e4bd20546f03e902de240253d
Soma Sekhar <Soma Sekhar@SERRENT>
2021-02-14 18:18:09 +0530

    BRM_LAB_2 First Commit.

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -3,7 +3,8 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println();
+		System.out.println("Hello, Today we are establishing communication between Maven and Jenkins.");
+		System.out.println("Thank you");
 	}
 
 }
